8521645ccf010a450d4626b9ca783459da6344bc
kyojunichi <junjundesuu@yahoo.co.jp>
2021-10-09 01:18:48 +0900

    Call Nomad coder movie API from movie app
    노마드 코더 映畫 API를 映畫 앱에서 呼出。

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
@@ -6,10 +7,7 @@ class App extends React.Component {
     movies: [],
   };
   componentDidMount() {
-    // 映畫 데이터 로딩中！
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 5000);
+    axios.get('https://yts-proxy.now.sh/list_movies.json');
   }
   render() {
     const { isLoading } = this.state;
