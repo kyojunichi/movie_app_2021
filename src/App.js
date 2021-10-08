c8bd99c8950634b286a9dea0f209bfffc299745e
kyojunichi <junjundesuu@yahoo.co.jp>
2021-10-09 00:38:10 +0900

    Realize loading phenomenon
    Loading 現象 具現。

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React from 'react';
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
   };
   componentDidMount() {
+    // 映畫 데이터 로딩中！
     setTimeout(() => {
       this.setState({ isLoading: false });
     }, 5000);
