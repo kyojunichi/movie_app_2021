a0edd705bcf33fdd488550f0990c21726cc185d4
kyojunichi <junjundesuu@yahoo.co.jp>
2021-10-09 00:28:33 +0900

    Display the loading status of the movie data
    映畫 데이터 로딩 狀態 表示

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
   render() {
-    return <div />;
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 }
 
