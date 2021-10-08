81579eb06726e723a989d55e14550991c4afc7d5
kyojunichi <junjundesuu@yahoo.co.jp>
2021-10-09 00:34:49 +0900

    Realize loading phenomenon
    Loading 現象 具現。

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ class App extends React.Component {
   state = {
     isLoading: true,
   };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000);
+  }
   render() {
     const { isLoading } = this.state;
     return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
