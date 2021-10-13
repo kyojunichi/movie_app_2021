42e305a5e0c329a5986715026a78359d2a390c4e
kyojunichi <junjundesuu@yahoo.co.jp>
2021-10-13 22:55:01 +0900

    Decorate title with style attributes
    style 屬性으로 title 꾸미기。

--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,9 @@ function Movie({ title, year, summary, poster }) {
         <div class="movie">
             <img src={poster} alt={title} title={title} />
             <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
+                <h3 class="movie__title" style={{ backgroundColor: 'red' }}>
+                    {title}
+                </h3>
                 <h5 class="movie__year">{year}</h5>
                 <p class="movie__summary">{summary}</p>
             </div>
