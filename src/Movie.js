4b7a8baf1ffd84ccbbbba9b2aa62440a91f2d2db
kyojunichi <junjundesuu@yahoo.co.jp>
2021-10-16 03:12:27 +0900

    Print out movie genre
    映畫 장르 出力。

--- src/Movie.js
+++ src/Movie.js
@@ -2,15 +2,20 @@ import React from "react";
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
     return (
         <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
-                <h3 className="movie__title">
-                    {title}
-                </h3>
+                <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre) => {
+                        return(
+                            <li className="movie__genre">{genre}</li>
+                        )
+                    })}
+                </ul>
                 <p className="movie__summary">{summary}</p>
             </div>
         </div>
