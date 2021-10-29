3e27fea6564ae1e592506ec1fd5ab56f87d3fb51
kyojunichi <junjundesuu@yahoo.co.jp>
2021-10-29 18:53:59 +0900

    App.js back to before version
    App.js 다시 元來대로 復元。

--- src/App.js
+++ src/App.js
@@ -7,15 +7,8 @@ import Home from "./routes/Home";
 function App() {
   return (
     <HashRouter>
-      <Route path="/home">
-        <h1>Home</h1>
-      </Route>
-      <Route path="/home/introduction">
-        <h1>Introduction</h1>
-      </Route>
-      <Route path="/about">
-        <h1>About</h1>
-      </Route>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
     </HashRouter>
   );
 }
