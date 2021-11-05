10bd8610393cdea2cfe3917ff78b348b0c61736b
kyojunichi <junjundesuu@yahoo.co.jp>
2021-11-06 00:10:54 +0900

    Use push() function
    push 函數 使用。

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,6 +3,9 @@ import React from "react";
 class Detail extends React.Component {
     componentDidMount() {
         const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push('/');
+        }
     }
 
     render() {
